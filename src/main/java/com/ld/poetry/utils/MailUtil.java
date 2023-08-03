7de9e09041c7cfe59c0433218be2b3129a4095ce
Sara <1622138424@qq.com>
2023-08-03 14:58:00 +0800

    搜索，历史记录等

--- src/main/java/com/ld/poetry/utils/MailUtil.java
+++ src/main/java/com/ld/poetry/utils/MailUtil.java
@@ -65,7 +65,7 @@ public class MailUtil {
     @PostConstruct
     public void init() {
         this.mailText = "<div style=\"font-family: serif;line-height: 22px;padding: 30px\">\n" +
-                "    <div style=\"display: flex;justify-content: center;width: 100%%;max-width: 900px;background-image: url('" + downloadUrl + "');background-size: cover;border-radius: 10px\"></div>\n" +
+                "    <div style=\"display: flex;justify-content: center;width: 100%%;max-width: 900px;background-image: url('https://s1.ax1x.com/2022/12/04/zsKgDs.jpg');background-size: cover;border-radius: 10px\"></div>\n" +
                 "    <div style=\"margin-top: 20px;display: flex;flex-direction: column;align-items: center\">\n" +
                 "        <div style=\"margin: 10px auto 20px;text-align: center\">\n" +
                 "            <div style=\"line-height: 32px;font-size: 26px;font-weight: bold;color: #000000\">\n" +
